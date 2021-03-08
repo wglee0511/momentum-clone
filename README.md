dd01aded823432ccd44f9859547d674d37bd5d82
soheeoott <soheeoott@gmail.com>
2021-03-09 06:01:26 +0900

    add clock.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 21-03-09
 - [x] 현재시간 시:분:초
+
+https://soheeoott.github.io/momentum-clone/
--- js/clock.js
+++ js/clock.js
@@ -1,16 +1,20 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = document.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
-    const minutes = date.getMinutes();
     const hours = date.getHours();
+    const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = 
+        `${hours < 10 ? `0${hours}` : hours}:${
+            minutes < 10 ? `0${minutes}` : minutes}:${
+                seconds < 10 ? `0${seconds}` : seconds}`; // 00~09 처리
 }
 
 function init () {
     getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
